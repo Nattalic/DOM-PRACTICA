930e52519f41daaacff24aafec871ba37771d9bf
Nattalic <nordonez376@gmail.com>
2025-08-26 12:51:22 -0500

    code

--- ejemplo de kevin/index.js
+++ ejemplo de kevin/index.js
@@ -49,3 +49,51 @@ console.dir ({
     input2,
     button
 })
+
+//eventos
+
+//cambiar el boton al ser presionado
+function activarMensaje () {
+    console.log("Boton presionado!!!")
+}
+
+changeText.addEventListener('click', () => {
+    const textElement = document.querySelector('.text')
+    textElement.textContent = 'iwiwiwiwiw' //cambiar el contenido del html desde el DOM
+    console.log(textElement)
+    textElement.setAttribute('style', 'color:pink') //se crea un atributo (cualquiera que uno quiera) a ese elemento en especifico 
+})
+
+
+//agregar elementos dandole click a un boton
+
+addElement.addEventListener('mouseover', () => {
+    const newItem = document.createElement('li') //crear elementos especificados directos de js
+    newItem.className = 'item' //especificar su atributo class dentro del elemento
+    newItem.textContent = `Item  ${itemList.children.length + 1}`  //(para que salga item 1 item 2 item 3 item 4...)
+    itemList.appendChild(newItem)
+    itemList.scrollTo(0,itemList.scrollHeight) //alto maximo que tiene el scroll 
+    // scroll to : sirve para desplazarse de manera vertical y horizontal (la barrita de subir y bajar)
+})
+
+
+//modo oscuro
+let darkMode =  true
+
+toggleClass.addEventListener('click', () => {
+    content.classList.toggle('highlight')
+    container.style = `background-color :  ${darkMode ? 'pink' : 'white'}`
+    //!true = false      !false = true
+    darkMode = !darkMode
+})
+
+form.addEventListener('submit', (evento) =>  {
+    evento.preventDefault() //evitar que la pagina se recarge 
+    console.log("formulario enviado")
+    const input1 = inputField.value
+    const input2 = myCheckbox.checked
+
+    alert(`Mensaje ingresado: ${input1} Verifico?  ${input2 ? 'Si' : 'No'}`) //form
+
+    form.reset() //limpiar los campos del form
+})
